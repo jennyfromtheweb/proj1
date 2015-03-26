6623e00200fa28ba849397a4f6d638dbaeba1919
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-03-26 13:23:23 -0700

    finished all required parts

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -21,4 +21,26 @@ class PokemonsController < ApplicationController
         redirect_to "/trainers/" + trainer_id.to_s
     end
 
+    def new
+        @pokemon = Pokemon.new
+    end
+
+    def create
+        @pokemon = Pokemon.create(pokemon_params)
+        @pokemon.health = 100
+        @pokemon.level = 1
+        @pokemon.trainer_id = current_trainer.id
+        if @pokemon.save
+            redirect_to "/trainers/" + @pokemon.trainer_id.to_s
+        else
+            render "new"
+        end
+    end
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit(:name)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   resources :pokemons
   patch "capture", to: "pokemons#capture", as: :capture
   patch "damage", to: "pokemons#damage", as: :damage
+
+  get "/pokemons/new", to: "pokemons#new"
+  post "pokemons", to: "pokemons#create"
 end
